8d9c0b5f30ef90363d2ca6327073a66c6fd25a2e
DESKTOP-AMC8OOC\Sunnyngai <arunwongdotw@gmail.com>
2018-06-01 09:49:03 +0700

    Add variable for report.

--- www/js/services/myService.js
+++ www/js/services/myService.js
@@ -8,6 +8,9 @@ angular.module('starter')
     this.appLanguage = {}; // object เก็บภาษา
     this.memberDetailFromLogin = {}; // object เก็บข้อมูล member หลัง Login
     this.questionSetDetail = {}; // object เก็บข้อมูล question set เอาไว้สร้าง question
-    this.allQuestionInSet = {}; // object เก็บข้อมูล question ทั้งหมดใน set นั้น
+    this.allQuestionInSet = {}; // object เก็บข้อมูล question ทั้งหมดใน set นั้น ใช้ในหน้า score
     this.inputDialog = {}; // object เก็บข้อมูล input จาก input dialog
+    this.countScorePerQuestion = {}; // object เก็บ score ทั้งหมดต่อ Question
+    this.countScorePerSet = {}; // object เก็บ score ทั้งหมดต่อ Set
+    this.chartType = {}; // object เก็บประเภทของกราฟ
   });
