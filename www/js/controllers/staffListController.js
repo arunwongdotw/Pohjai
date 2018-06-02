1e58075a769b4c125d228ce31386e00612f57759
DESKTOP-AMC8OOC\Sunnyngai <arunwongdotw@gmail.com>
2018-06-02 15:47:35 +0700

    Add new state staff-list.

---
+++ www/js/controllers/staffListController.js
@@ -0,0 +1,45 @@
+appControllers.controller('staffListCtrl', function($scope, $timeout, $state, $stateParams, $ionicHistory, $ionicPlatform, $http, myService, $mdDialog) {
+  $scope.appLanguage = {};
+  $scope.staffList = myService.staffList;
+
+  if (typeof window.localStorage.appLanguageID == 'undefined') {
+    $scope.appLanguageID = "1";
+    getAppLanguage();
+  } else if ((window.localStorage.appLanguageID != "") || (window.localStorage.appLanguageID != null)) {
+    $scope.appLanguageID = window.localStorage.appLanguageID;
+    getAppLanguage();
+  } else {
+    $scope.appLanguageID = "1";
+    getAppLanguage();
+  }
+
+  function getAppLanguage() {
+    $http.get(myService.configAPI.webserviceURL + 'webservices/getAppLanguage.php?appLanguageID=' + $scope.appLanguageID)
+      .then(function(response) {
+        $scope.appLanguage = response.data.results[0];
+      }, function(error) {
+        console.log(error);
+      });
+  }
+
+  $scope.navigateTo = function(stateName) {
+    $timeout(function() {
+      if ($ionicHistory.currentStateName() != stateName) {
+        $ionicHistory.nextViewOptions({
+          disableAnimate: false,
+          disableBack: true
+        });
+        $state.go(stateName);
+      }
+    }, ($scope.isAnimated ? 300 : 0));
+  };
+
+  $scope.btnBack = function() {
+    navigator.app.backHistory();
+  };
+
+  $scope.btnNavigateToScore = function(staff) {
+    myService.staffDetail = staff;
+    $state.go('menu2.score');
+  };
+});
