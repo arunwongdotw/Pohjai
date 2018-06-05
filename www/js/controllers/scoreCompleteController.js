f64d3ad64ac9a62b97c561b437eb0283ad666983
DESKTOP-AMC8OOC\Sunnyngai <arunwongdotw@gmail.com>
2018-06-05 13:38:28 +0700

    Add random advertise and add advertise frequency.

--- www/js/controllers/scoreCompleteController.js
+++ www/js/controllers/scoreCompleteController.js
@@ -37,9 +37,84 @@ appControllers.controller('scoreCompleteCtrl', function($scope, $timeout, $state
     $scope.navigateTo('menu2.score');
   };
 
+  $scope.adsArray = [];
+  var arrayOfRandomNumber = [];
+  var randomNumber;
+  var allAdsLength;
+  var checkDup;
+
   $scope.$on('$ionicView.enter', function() {
+    getAllAds(function(status) {
+      createAdsArray(function(status) {});
+      addAdFrequency(function(status) {});
+    });
     $timeout(function() {
       $scope.navigateTo('menu2.score');
-    }, 5000);
+    }, 10000);
   });
+
+  function getAllAds(callback) {
+    $http.get(myService.configAPI.webserviceURL + 'webservices/getAllAds.php')
+      .then(function(response) {
+        $scope.allAds = response.data.results;
+        callback();
+      }, function(error) {
+        console.log(error);
+      });
+  }
+
+  function createAdsArray(callback) {
+    for (var i = 0; i < 2; i++) {
+      getRandomNumber(function(status) {
+        pushAdsArray(randomNumber, function(status) {
+          callback();
+        });
+      });
+    }
+  }
+
+  function getRandomNumber(callback) {
+    allAdsLength = $scope.allAds.length;
+    randomNumber = Math.floor(Math.random() * allAdsLength);
+    if (arrayOfRandomNumber.length == 0) {
+      arrayOfRandomNumber.push(randomNumber);
+      callback(randomNumber);
+    } else {
+      checkDup = checkDupInArrayOfRandomNumber(randomNumber);
+      if (checkDup == true) {
+        getRandomNumber(callback);
+      } else {
+        arrayOfRandomNumber.push(randomNumber);
+        callback(randomNumber);
+      }
+    }
+  }
+
+  function pushAdsArray(randomNumber, callback) {
+    $scope.adsArray.push($scope.allAds[randomNumber]);
+    callback();
+  }
+
+  function checkDupInArrayOfRandomNumber(randomNumber) {
+    for (var j = 0; j < arrayOfRandomNumber.length; j++) {
+      if (arrayOfRandomNumber[j] == randomNumber) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function addAdFrequency(callback) {
+    for (var i = 0; i < $scope.adsArray.length; i++) {
+      var frequency = parseInt($scope.adsArray[i].advertise_frequency);
+      frequency = frequency + 1;
+      $http.get(myService.configAPI.webserviceURL + 'webservices/addAdFrequency.php?adID=' + $scope.adsArray[i].advertise_id + '&frequency=' + frequency)
+        .then(function(response) {
+          console.log(response);
+        }, function(error) {
+          console.log(error);
+        });
+    }
+    callback();
+  }
 });
