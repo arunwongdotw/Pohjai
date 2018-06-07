50af66d0cc8d78546732d8190c6ba3b6f05c12a3
DESKTOP-AMC8OOC\Sunnyngai <arunwongdotw@gmail.com>
2018-06-07 10:43:26 +0700

    Add delete comment function.

--- www/js/controllers/commentListController.js
+++ www/js/controllers/commentListController.js
@@ -45,4 +45,82 @@ appControllers.controller('commentListCtrl', function($scope, $timeout, $state, 
   $scope.btnBack = function() {
     navigator.app.backHistory();
   };
+
+  $scope.btnDelComment = function(comment) {
+    if ($scope.appLanguageID == "1") {
+      $mdDialog.show({
+        controller: 'DialogController',
+        templateUrl: 'confirm-dialog.html',
+        locals: {
+          displayOption: {
+            title: "ลบความคิดเห็นนี้ ?",
+            content: "คุณแน่ใจที่จะลบความคิดเห็นนี้",
+            ok: "ตกลง",
+            cancel: "ยกเลิก"
+          }
+        }
+      }).then(function(response) {
+        $http({
+          url: myService.configAPI.webserviceURL + 'webservices/deleteComment.php',
+          method: 'POST',
+          data: {
+            var_commentid: comment.comment_id
+          }
+        }).then(function(response) {
+          $mdDialog.show({
+            controller: 'DialogController',
+            templateUrl: 'confirm-dialog.html',
+            locals: {
+              displayOption: {
+                title: "ลบความคิดเห็นนี้สำเร็จ !",
+                content: "คุณลบความคิดเห็นนี้สำเร็จ",
+                ok: "ตกลง"
+              }
+            }
+          }).then(function(response) {
+            $state.go('menu2.question');
+          });
+        }, function(error) {
+          console.log(error);
+        });
+      });
+    } else {
+      $mdDialog.show({
+        controller: 'DialogController',
+        templateUrl: 'confirm-dialog.html',
+        locals: {
+          displayOption: {
+            title: "Delete Comment ?",
+            content: "Are you sure to delete this comment ?",
+            ok: "Confirm",
+            cancel: "Cancel"
+          }
+        }
+      }).then(function(response) {
+        $http({
+          url: myService.configAPI.webserviceURL + 'webservices/deleteComment.php',
+          method: 'POST',
+          data: {
+            var_commentid: comment.comment_id
+          }
+        }).then(function(response) {
+          $mdDialog.show({
+            controller: 'DialogController',
+            templateUrl: 'confirm-dialog.html',
+            locals: {
+              displayOption: {
+                title: "Delete Form Successfully",
+                content: "You deleted form successfully.",
+                ok: "Confirm"
+              }
+            }
+          }).then(function(response) {
+            $state.go('menu2.question');
+          });
+        }, function(error) {
+          console.log(error);
+        });
+      });
+    }
+  };
 });
